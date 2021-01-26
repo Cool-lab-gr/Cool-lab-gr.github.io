fe04f03d46be5b7105661442768c5a6d8a84a180
coolLaboratory <cool.lab.gr@gmail.com>
2021-01-26 16:46:56 +0200

    create a simple layout and add anmations

---
+++ layout.js
@@ -0,0 +1,41 @@
+/**@type {KeyframeAnimationOptions} options */
+const headerOptions = {
+  duration: 350,
+  iterations: 1,
+  fill: 'forwards',
+}
+/**@type {Keyframe[] | PropertyIndexedKeyframes} keyframes */
+const headerKeyframes = [
+  { transform: 'translateY(0%)', composite: "auto" }
+]
+
+
+document.querySelectorAll('.box').forEach(box => {
+
+  box.animate([
+    {opacity: 1, offset: .3 }
+  ])
+
+})
+
+
+setTimeout(() => {
+  document.querySelector("header").animate(headerKeyframes, headerOptions);
+
+  document.querySelectorAll('.box').forEach((box, key) => {
+
+    setTimeout(() => {
+      box.animate([
+        {opacity: 1}
+      ], {
+        duration: 350,
+        iterations: 1,
+        fill: 'forwards',
+      })
+    }, 200 * ++key)
+  })
+}, 500)
+
+
+
+
